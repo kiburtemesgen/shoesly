78cf1a69118bbfa5343e7c9434adceb73c940c63
kiburtemesgen <kiburezelast@gmail.com>
2023-09-18 12:13:32 +0300

    remove print from code

--- lib/core/utils/format_review_date.dart
+++ lib/core/utils/format_review_date.dart
@@ -1,7 +1,6 @@
 import 'package:intl/intl.dart';
 
 String formatReviewDate(DateTime date) {
-  print('the param date is: ${date.toString()}');
   final now = DateTime.now();
   final today = DateTime(now.year, now.month, now.day);
   final yesterday = today.subtract(const Duration(days: 1));
