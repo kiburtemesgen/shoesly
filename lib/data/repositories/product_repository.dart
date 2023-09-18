fcd09913821434ac2f4d42579e553fd8edcfdcb1
kiburtemesgen <kiburezelast@gmail.com>
2023-09-18 12:14:13 +0300

    create order repo

--- lib/data/repositories/product_repository.dart
+++ lib/data/repositories/product_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:prior_soft/data/models/filter_product_request.dart';
+import 'package:prior_soft/data/models/order_model.dart';
 import 'package:prior_soft/data/models/product_model.dart';
 import 'package:prior_soft/data/models/review_model.dart';
 import 'package:prior_soft/data/services/product_service.dart';
@@ -7,6 +8,7 @@ import 'package:prior_soft/data/services/product_service.dart';
 abstract class ProductRepository {
   Future<Either<Error, List<ProductModel>>> getProducts(int pageNumber, String brand, int total);
   Future<Either<Error, List<ProductModel>>> getFilteredProducts(FilterProductRequest filterProductRequest, int pageNumber);
+  Future<Either<Error, Unit>> createOrder(OrderModel order);
   Future<Either<Error, List<ReviewModel>>> getReviews(String productId, int rating);
   Future<Either<Error, List<ReviewModel>>> getTopReviews(String productId);
 }
@@ -40,5 +42,11 @@ class ProductRepositoryImpl extends ProductRepository {
   Future<Either<Error, List<ReviewModel>>> getTopReviews(String productId)async {
     final response = await productService.getTopReviews(productId);
    return response.fold((error) => Left(error), (result) => Right(result));
+  }
+  
+  @override
+  Future<Either<Error,Unit>> createOrder(OrderModel order) async{
+    final response = await productService.createOrder(order);
+    return response.fold((error) => Left(error), (result) => Right(result));
   }
 }
